9de61555b69430a1e1e6107cf26aa63a9bcacda9
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-14 00:04:18 +0000

    how to handle a GET request

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require("express");
 // set up express app
 const app = express();
 
+app.get('/', function(req, res){
+	console.log('GET request');
+	res.end();
+});
+
 // listen for requests
 app.listen(process.env.port || 8080, function(){
 	console.log('now listening for requests');
